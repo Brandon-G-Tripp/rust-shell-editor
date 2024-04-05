312c69753a21d4fae0b83d5fcdccabc0217d6108
Brandon Tripp <brandongtripp@gmail.com>
2024-04-05 17:58:07 -0600

    LinkedList Implementation of constructor, is_empty and len

--- src/data_structures/linked_list.rs
+++ src/data_structures/linked_list.rs
@@ -1,3 +1,33 @@
+pub struct Node<T> {
+    element: T, 
+    next: Option<Box<Node<T>>>,
+}
+
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+    size: usize,
+} 
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList {
+            head: None, 
+            size: 0,
+        } 
+    } 
+
+    pub fn len(&self) -> usize {
+        self.size
+    } 
+
+    pub fn is_empty(&self) -> bool {
+        self.size == 0
+    } 
+} 
+
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -8,4 +38,6 @@ mod tests {
         assert_eq!(list.len(), 0);
         assert!(list.is_empty());
     } 
+
+
 } 
