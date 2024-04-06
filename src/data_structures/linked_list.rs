3fea947792ddd8350678d5f2579385e0d4064bb9
Brandon Tripp <brandongtripp@gmail.com>
2024-04-05 20:51:11 -0600

    WIP: Implementation of methods on linked list compiling but failing tests

--- src/data_structures/linked_list.rs
+++ src/data_structures/linked_list.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct Node<T> {
     element: T, 
     next: Option<Box<Node<T>>>,
@@ -23,7 +25,91 @@ impl<T> LinkedList<T> {
     pub fn is_empty(&self) -> bool {
         self.size == 0
     } 
+
+    pub fn push_front(&mut self, element: T) {
+        let new_node = Box::new(Node {
+            element, 
+            next: self.head.take(),
+        });
+        self.head = Some(new_node);
+        self.size += 1;
+    } 
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            self.size -= 1;
+            node.element
+        })
+    }
+
+    pub fn push_back(&mut self, element: T) {
+        let new_node = Box::new(Node {
+            element,
+            next: None,
+        });
+
+        if let Some(mut last_node) = self.head.as_mut() {
+            while let Some(next_node) = mem::take(&mut last_node.next) {
+                *last_node = next_node;
+            } 
+            last_node.next = Some(new_node);
+        } else {
+            self.head = Some(new_node);
+        } 
+        self.size += 1;
+    } 
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        match self.head.take() {
+            Some(mut node) => {
+                if node.next.is_none() {
+                    self.size -= 1;
+                    Some(node.element)
+                } else {
+                    let mut current = &mut node;
+                    while let Some(ref mut next_node) = current.next {
+                        if next_node.next.is_none() {
+                            let last_node = next_node.next.take().unwrap();
+                            self.size -= 1;
+                            return Some(last_node.element);
+                        }
+                        current = next_node;
+                    }
+                    self.head = Some(node);
+                    None
+                }
+            }
+            None => None,
+        }
+    } 
+
+    pub fn iter(&self) -> LinkedListIter<T> {
+        LinkedListIter {
+            current: self.head.as_deref(),
+        } 
+    } 
+
+    pub fn clear(&mut self) {
+        self.head = None;
+        self.size = 0;
+    } 
 } 
+
+pub struct LinkedListIter<'a, T> {
+    current: Option<&'a Node<T>>,
+} 
+
+impl<'a, T> Iterator for LinkedListIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current.map(|node| {
+            self.current = node.next.as_deref();
+            &node.element
+        })
+    } 
+}
 
 
 
