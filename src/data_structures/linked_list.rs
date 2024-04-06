9124b29f5707fad5d5dbc9f1a1ace6e1d616a6fd
Brandon Tripp <brandongtripp@gmail.com>
2024-04-05 18:08:12 -0600

    Tests for driving development of push_front, pop_front, push_back, pop_back, iter and clear

--- src/data_structures/linked_list.rs
+++ src/data_structures/linked_list.rs
@@ -39,5 +39,52 @@ mod tests {
         assert!(list.is_empty());
     } 
 
+    #[test]
+    fn test_push_front_and_pop_front() {
+        let mut list = LinkedList::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+        assert!(list.is_empty());
+    } 
 
+    #[test]
+    fn test_push_back_and_pop_bac() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
+        assert!(list.is_empty());
+    } 
+
+    #[test]
+    fn test_iter() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), None);
+    } 
+
+    #[test]
+    fn test_clear() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        list.clear();
+        assert!(list.is_empty());
+    } 
 } 
