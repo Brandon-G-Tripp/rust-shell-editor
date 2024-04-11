72a7e929d1aeb4bc77cec415b8907a4ceba427e2
Brandon Tripp <brandongtripp@gmail.com>
2024-04-11 13:33:30 -0600

    feat: Change the main entry point to pass stdin(reader)/stdout(writer)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::{self, Write};
+
+use upepo::io::{input, output};
+
 fn main() {
-    println!("Hello, world!");
+    // Read cmd line args
+    let args = input::read_args();
+    println!("Command line arguments: {:?}", args);
+
+    // Prompt the user for input
+    print!("Enter a command: ");
+    io::stdout().flush().unwrap();
+    match input::read_line(&mut io::stdin().lock()) {
+        Ok(command) => {
+            println!("Entered command: {}", command);
+        }
+        Err(err) => {
+            eprintln!("Error reading input: {}", err);
+        }
+    } 
+
+    // Write output to stdout
+    let message = "Hello, world!";
+    match output::write_output(&mut io::stdout(), message) {
+        Ok(_) => println!("Output written successfully"),
+        Err(err) => eprintln!("Error writing output: {}", err),
+    } 
+
+    // Format output with color
+    let colored_message = output::format_output("Colored message", "31");
+    println!("{}", colored_message);
 }
