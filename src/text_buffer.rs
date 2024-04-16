f223a069370cb72e06e9124a7c7bb31d39b14bc6
Brandon Tripp <brandongtripp@gmail.com>
2024-04-15 18:26:40 -0600

    Tests for driving development of the text buffer methods and the delete char, get lines methods

--- src/text_buffer.rs
+++ src/text_buffer.rs
@@ -12,6 +12,18 @@ impl TextBuffer {
     pub fn insert_char(&mut self, pos: usize, ch: char) {
         self.content.insert(pos, ch);
     } 
+
+    pub fn delete_char(&mut self, pos: usize) {
+        self.content.remove(pos);
+    } 
+
+    pub fn get_line(&self, line_num: usize) -> Option<&str> {
+        self.content.lines().nth(line_num)
+    }
+
+    pub fn get_lines(&self) -> Vec<&str> {
+        self.content.lines().collect()
+    }
 } 
 
 #[cfg(test)]
@@ -34,4 +46,28 @@ mod tests {
         buffer.insert_char(4, 'o');
         assert_eq!(buffer.content, "Hello");
     }
+
+    #[test]
+    fn test_delete_char() {
+        let mut buffer = TextBuffer::new();
+        buffer.content = "Hello".to_string();
+        buffer.delete_char(1);
+        assert_eq!(buffer.content, "Hllo");
+    } 
+
+    #[test]
+    fn test_get_line() {
+        let mut buffer = TextBuffer::new();
+        buffer.content = "Hello\nWorld".to_string();
+        assert_eq!(buffer.get_line(0), Some("Hello"));
+        assert_eq!(buffer.get_line(1), Some("World"));
+        assert_eq!(buffer.get_line(2), None);
+    } 
+
+    #[test]
+    fn test_get_lines() {
+        let mut buffer = TextBuffer::new();
+        buffer.content = "Hello\nWorld".to_string();
+        assert_eq!(buffer.get_lines(), vec!["Hello", "World"]);
+    } 
 } 
